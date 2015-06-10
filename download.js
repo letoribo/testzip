5c23352e104afe5fa58e2f9289fe429fdf40ac46
letoribo <lawegard@yandex.ru>
2015-06-10 12:11:15 +0300

    testing udondan:jszip

---
+++ download.js
@@ -0,0 +1,16 @@
+if (Meteor.isClient) {
+  
+  Template.hello.helpers({
+
+  });
+
+  Template.hello.events({
+    
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
